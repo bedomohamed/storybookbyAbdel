dad60a04c6fa452e36a56f3c54a29b4c7b87faa5
abdelmon <abdelmon@umich.edu>
2022-05-27 19:15:26 -0400

    cleaning cards.stories and button component

--- src/Components/Card/Card.stories.tsx
+++ src/Components/Card/Card.stories.tsx
@@ -5,10 +5,6 @@ export default {
     
 }
 
-// const Template = args => <Card     
-// img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-// title="Simple Risotto"
-// description="Fear Risotto no more! This simple recipe is perfect for family dinners."/>
 const Template = (args) => <Card {...args} />;
 
 export const Default = Template.bind({});
--- src/Components/Generic/Button/Button.test.tsx
+++ src/Components/Generic/Button/Button.test.tsx
@@ -3,9 +3,6 @@ import { render, screen} from "@testing-library/react"
 import  { Primary } from "./Button.stories"; 
 
 test('should render', () => {
-    // render(<Button label="Click Me"/>);
-    // expect(screen.getByRole("button")).toHaveTextContent(/click/i);
-
     render(<Primary {...Primary.args}/>);
     expect(screen.getByRole("button")).toHaveTextContent(/button/i);
 
